db300812d5ee81ac4a87f6874beec9acbb6841df
marcelofms1 <43277551+marcelofms1@users.noreply.github.com>
2021-10-27 15:11:40 -0400

    Updated script.js
    If applied, this commit will create an array of Pokemons, including their name, height, type and abilities.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,49 @@
-alert('Hello World');
+let pokemonList = [
+    {name: 'Charizard',
+     height: 1.7,
+     type: ['fire', 'flying'],
+     abilities:['Blaze', 'Solar-Power']
+    },
 
-favoriteFood = 'Pizza';
-document.write(favoriteFood);
+    {name: 'Zapdos',
+     height: 1.6,
+     type: ['electric', 'flying'],
+     abilities:['Lightningrod', 'Pressure']
+    },
+
+    {name: 'Gyarados',
+     height: 6.5,
+     type: ['water', 'flying'],
+     abilities:['Intimidate', 'Moxie']
+    },
+
+    {name: 'Tyranitar',
+     height: 2,
+     type: ['dark', 'rock'],
+     abilities:['Sand-stream', 'Unnerve']
+    },
+
+    {name: 'Galvantula',
+     height: 0.8,
+     type: ['electric', 'bug'],
+     abilities:['Compoundeyes', 'Swarm', 'Unnerve']
+    },
+
+    {name: 'Scyther',
+     height: 1.5,
+     type:['grass', 'flying'],
+     abilities:['Steadfast', 'Swarm', 'Technician']
+    },
+
+    {name:'Gengar',
+     height: 1.5,
+     type: ['poison', 'ghost'],
+     abilities:['levitate', 'Psychic']
+    },
+
+    {name:'Aggron',
+     height: 2.1,
+     type:['rock', 'steel'],
+     abilities:['Rock-head', 'Heavy-metal']
+    },
+];
