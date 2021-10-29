ad35874ba3affe38e8dd02a37413e5df0830ded6
marcelofms1 <43277551+marcelofms1@users.noreply.github.com>
2021-10-29 12:45:49 -0400

    update script.js
    if applied, this commit will loop over the code and print pokemon with a certain height along with a message.

--- js/scripts.js
+++ js/scripts.js
@@ -48,8 +48,57 @@ let pokemonList = [
     },
 ];
 
-// This creates a loop of strings with name, height and types of pokemon.
+// This creates a loop of strings with name, height and abilities of pokemon.
+
+// for (let i = 0; i < pokemonList.length; i++){
+//     document.write(` ${pokemonList[i].name}- (Height: ${pokemonList[i].height}) (Type: ${pokemonList[i].abilities})`);   
+// } 
+
+
+let pokemonSize = [
+    big = `WOW, that's a big pokemon!`,
+    small = `Uh, this one is kind of small.`
+];
 
 for (let i = 0; i < pokemonList.length; i++){
-    document.write(`${pokemonList[i].name}- (Height: ${pokemonList[i].height}) (Type: ${pokemonList[i].abilities})`);   
+
+    if (pokemonList[i].height > 1.6){
+        document.write(` ${pokemonList[i].name} (height: ${pokemonList[i].height}) - ${pokemonSize[0]} `);
+    } 
+    else {
+        document.write(` ${pokemonList[i].name} (height: ${pokemonList[i].height}) - ${pokemonSize[1]} `);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// This code identifies big and small pokemon
+
+// let pokemonSize = [
+//     big = `Wow, that's a big Pokemon!`,
+//     small = `Uh, this Pokemon is kind of small.`
+// ];
+
+// for (let i = 0; i < pokemonList.length; i++){
+    
+//     if (pokemonList.height >= 1.7){
+//         document.write(` ${pokemonList[i].name} (height: ${pokemonList[i].height}) - ${pokemonSize[0]} `);
+//     } 
+//     else (pokemonList.height <= 1.5);{
+//         document.write(` ${pokemonList[i].name} (height: ${pokemonList[i].height}) - ${pokemonSize[1]}`);
+//     }
+// }
