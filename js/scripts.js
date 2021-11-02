3795c21dffef1caf5c194c4446687d90e0b4118d
marcelofms1 <43277551+marcelofms1@users.noreply.github.com>
2021-11-02 15:15:41 -0400

    update script.js
    if applied this commit will add the type and abilities of each pokemon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,50 @@
 let pokemonList = [
     {name: 'Charizard',
      height: 1.7,
-     type: ['fire', 'flying'],
-     abilities:['Blaze', 'Solar-Power']
+     type: ['Fire', ' Flying'],
+     abilities:['Blaze', ' Solar-Power']
     },
 
     {name: 'Zapdos',
      height: 1.6,
-     type: ['electric', 'flying'],
-     abilities:['Lightningrod', 'Pressure']
+     type: ['Electric', ' Flying'],
+     abilities:['Lightningrod', ' Pressure']
     },
 
     {name: 'Gyarados',
      height: 6.5,
-     type: ['water', 'flying'],
-     abilities:['Intimidate', 'Moxie']
+     type: ['Water', ' Flying'],
+     abilities:['Intimidate', ' Moxie']
     },
 
     {name: 'Tyranitar',
      height: 2,
-     type: ['dark', 'rock'],
-     abilities:['Sand-stream', 'Unnerve']
+     type: ['Dark', ' Rock'],
+     abilities:['Sand-stream', ' Unnerve']
     },
 
     {name: 'Galvantula',
      height: 0.8,
-     type: ['electric', 'bug'],
-     abilities:['Compoundeyes', 'Swarm', 'Unnerve']
+     type: ['Electric', ' Bug'],
+     abilities:['Compoundeyes', ' Swarm', ' Unnerve']
     },
 
     {name: 'Scyther',
      height: 1.5,
-     type:['grass', 'flying'],
-     abilities:['Steadfast', 'Swarm', 'Technician']
+     type:['Grass', ' Flying'],
+     abilities:['Steadfast', ' Swarm', ' Technician']
     },
 
     {name:'Gengar',
      height: 1.5,
-     type: ['poison', 'ghost'],
-     abilities:['levitate', 'Psychic']
+     type: ['Poison', ' Ghost'],
+     abilities:['Levitate', ' Psychic']
     },
 
     {name:'Aggron',
      height: 2.1,
-     type:['rock', 'steel'],
-     abilities:['Rock-head', 'Heavy-metal']
+     type:['Rock', ' Steel'],
+     abilities:['Rock-head', ' Heavy-metal']
     },
 ];
 
@@ -65,15 +65,19 @@ let pokemonSize = [
 
 // This code creates a for loop with if and else statements:
 
-
+function printArrayList(){
 for (let i = 0; i < pokemonList.length; i++){
 
     if (pokemonList[i].height > 1.6){
-        document.write(`<br>${pokemonList[i].name} - `);
-        document.write(`(Height: ${pokemonList[i].height}) ${pokemonSize[0]}<br>`);
+        document.write(`<p id="pokemon_name">${pokemonList[i].name} - </p> `);
+        document.write(`<p>(Height: ${pokemonList[i].height}) ${pokemonSize[0]}</p> <p>(Abilities: ${pokemonList[i].abilities})</p> <p>(Type: ${pokemonList[i].type})</p>`);
     } 
     else {
-        document.write(`<br>${pokemonList[i].name} - `);
-        document.write(`(Height: ${pokemonList[i].height}) ${pokemonSize[1]}<br>`)
+        document.write(`<p id="pokemon_name">${pokemonList[i].name} - </p>`);
+        document.write(`<p>(Height: ${pokemonList[i].height}) ${pokemonSize[1]}</p> <p>(Abilities: ${pokemonList[i].abilities})</p> <p>(Type: ${pokemonList[i].type})</p>`)
     }
 }
+}
+
+printArrayList();
+
