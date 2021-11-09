79e0df5feefa7369de6cbdf13fd57a0240d1ff6a
marcelofms1 <43277551+marcelofms1@users.noreply.github.com>
2021-11-09 13:05:49 -0500

    update js
    if applied, this code will have a simplified version of my previous IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonRepository = (function () {
-    let pokemonList = [
+    let repository = [
         {name: 'Charizard',
         height: 1.7,
         type: ['Fire', ' Flying'],
@@ -49,97 +49,68 @@ let pokemonRepository = (function () {
         }
     ];
 
-    function getAll(){
-        return pokemonList;
-    }
+    function add(pokemon) {
+        if (
+          typeof pokemon === "object" &&
+          "name" in pokemon &&
+          "height" in pokemon &&
+          "types" in pokemon
+        ) {
+          repository.push(pokemon);
+        } else {
+          console.log("pokemon is not correct");
+        }
+      }
+      function getAll() {
+        return repository;
+      }
 
-    function add(pokemon){
-        pokemonList.push(pokemon);
-    }
+      function showDetails(pokemon){
+          console.log(pokemon)
+      }
 
-    function myLoopFunction(pokemonList){
-    document.write(`<h3>(${pokemonList.name})</h3>`);
-    document.write(`<p>Height: ${pokemonList.height}.</p>`);
-    document.write(`<p>Type: ${pokemonList.type}.</p>`);
-    document.write(`<p>Abilities: ${pokemonList.abilities}.</p>`);
-    }
-    pokemonList.forEach(myLoopFunction);
+    //   This loop will go over my pokemon and show them of the page inside buttons
+    
+      function addListItem(pokemon){
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listpokemon = document.createElement("li");
+        let button = document.createElement("button");
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        listpokemon.appendChild(button);
+        pokemonList.appendChild(listpokemon);
 
-    return {
-        getAll: getAll,
+        button.addEventListener('click', function(event){
+            showDetails(pokemon)
+        });
+      }
+
+    //   This code returns function
+      return {
         add: add,
-        myLoopFunction: myLoopFunction
-    };
-})();
+        getAll: getAll,
+        addListItem: addListItem,
+        showDetails: showDetails
+      };
+    })();
+    
+    pokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
+    
+    console.log(pokemonRepository.getAll());
+    
+    pokemonRepository.getAll().forEach(function (pokemon) {
+      pokemonRepository.addListItem(pokemon);
+    });
+    
 
 
 
-pokemonRepository.getAll().forEach(function(pokemonList) {
-    pokemonRepository.myLoopFunction(pokemonList);
-});
-
-
-
-
-
-
-
-// let pokemonList = [
-//     {name: 'Charizard',
-//      height: 1.7,
-//      type: ['Fire', ' Flying'],
-//      abilities:['Blaze', ' Solar-Power']
-//     },
-
-//     {name: 'Zapdos',
-//      height: 1.6,
-//      type: ['Electric', ' Flying'],
-//      abilities:['Lightningrod', ' Pressure']
-//     },
-
-//     {name: 'Gyarados',
-//      height: 6.5,
-//      type: ['Water', ' Flying'],
-//      abilities:['Intimidate', ' Moxie']
-//     },
-
-//     {name: 'Tyranitar',
-//      height: 2,
-//      type: ['Dark', ' Rock'],
-//      abilities:['Sand-stream', ' Unnerve']
-//     },
-
-//     {name: 'Galvantula',
-//      height: 0.8,
-//      type: ['Electric', ' Bug'],
-//      abilities:['Compoundeyes', ' Swarm', ' Unnerve']
-//     },
-
-//     {name: 'Scyther',
-//      height: 1.5,
-//      type:['Grass', ' Flying'],
-//      abilities:['Steadfast', ' Swarm', ' Technician']
-//     },
-
-//     {name:'Gengar',
-//      height: 1.5,
-//      type: ['Poison', ' Ghost'],
-//      abilities:['Levitate', ' Psychic']
-//     },
-
-//     {name:'Aggron',
-//      height: 2.1,
-//      type:['Rock', ' Steel'],
-//      abilities:['Rock-head', ' Heavy-metal']
-//     },
-// ];
-
-
-// // This code creates a for loop using .forEach function
-// function myLoopFunction(pokemonList){
-//     document.write(`<br>(${pokemonList.name})<br>`);
-//     document.write(`Height: ${pokemonList.height}.`);
-//     document.write(`Type: ${pokemonList.type}.`);
-//     document.write(`Abilities: ${pokemonList.abilities}.`);
-// }
-// pokemonList.forEach(myLoopFunction);
+// pokemonList.forEach(function(pokemon){
+        // let pokemonList = document.querySelector('.pokemon-list');
+        // let listpokemon = document.createElement('li');
+        // let button = document.createElement('button');
+        // button.innerText = pokemon.name;
+        // button.classList.add("button-class");
+        // listpokemon.appendChild(button);
+        // pokemonList.appendChild(listpokemon);
+    // });
