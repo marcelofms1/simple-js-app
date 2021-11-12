608b48c772bac6fceaba02e0eaf19deee65e9b4a
marcelofms1 <43277551+marcelofms1@users.noreply.github.com>
2021-11-12 13:21:20 -0500

    update js
    if applied, this commit will modify my pokemon repository with a fetch function, detailsurl

--- js/scripts.js
+++ js/scripts.js
@@ -1,104 +1,85 @@
 let pokemonRepository = (function () {
-    let repository = [
-        {name: 'Charizard',
-        height: 1.7,
-        type: ['Fire', ' Flying'],
-        abilities:['Blaze', ' Solar-Power']
-        },
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-        {name: 'Zapdos',
-        height: 1.6,
-        type: ['Electric', ' Flying'],
-        abilities:['Lightningrod', ' Pressure']
-        },
-
-        {name: 'Gyarados',
-        height: 6.5,
-        type: ['Water', ' Flying'],
-        abilities:['Intimidate', ' Moxie']
-        },
-
-        {name: 'Tyranitar',
-        height: 2,
-        type: ['Dark', ' Rock'],
-        abilities:['Sand-stream', ' Unnerve']
-        },
-
-        {name: 'Galvantula',
-        height: 0.8,
-        type: ['Electric', ' Bug'],
-        abilities:['Compoundeyes', ' Swarm', ' Unnerve']
-        },
-
-        {name: 'Scyther',
-        height: 1.5,
-        type:['Grass', ' Flying'],
-        abilities:['Steadfast', ' Swarm', ' Technician']
-        },
-
-        {name:'Gengar',
-        height: 1.5,
-        type: ['Poison', ' Ghost'],
-        abilities:['Levitate', ' Psychic']
-        },
-
-        {name:'Aggron',
-        height: 2.1,
-        type:['Rock', ' Steel'],
-        abilities:['Rock-head', ' Heavy-metal']
-        }
-    ];
-
-    function add(pokemon) {
-        if (
-          typeof pokemon === "object" &&
-          "name" in pokemon &&
-          "height" in pokemon &&
-          "types" in pokemon
-        ) {
-          repository.push(pokemon);
-        } else {
-          console.log("pokemon is not correct");
-        }
-      }
-      function getAll() {
-        return repository;
-      }
-
-      function showDetails(pokemon){
-          console.log(pokemon)
-      }
-
-    //   This loop will go over my pokemon and show them of the page inside buttons
-    
-      function addListItem(pokemon){
-        let pokemonList = document.querySelector(".pokemon-list");
-        let listpokemon = document.createElement("li");
-        let button = document.createElement("button");
-        button.innerText = pokemon.name;
-        button.classList.add("button-class");
-        listpokemon.appendChild(button);
-        pokemonList.appendChild(listpokemon);
-
-        button.addEventListener('click', function(event){   console.log(pokemon)
-            showDetails(pokemon)
-        });
-      }
-
-    //   This code returns function
-      return {
-        add: add,
-        getAll: getAll,
-        addListItem: addListItem,
-        showDetails: showDetails
-      };
-    })();
-    
-    pokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
-    
-    console.log(pokemonRepository.getAll());
-    
-    pokemonRepository.getAll().forEach(function (pokemon) {
-      pokemonRepository.addListItem(pokemon);
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
+    }
+  }
+  function getAll() {
+    return pokemonList;
+  }
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+    button.addEventListener("click", function(event) {
+      showDetails(pokemon);
     });
-    
+  }
+
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        console.log(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function () {
+      console.log(item);
+    });
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
+  };
+})();
+
+
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
+});
+
+
